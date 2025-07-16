9aed5df9ee95279d64c0fcf8b3522a4a519aaa75
Leon Scherer <55341374+Fruup@users.noreply.github.com>
2025-07-16 19:51:01 +0200

    fix: add allowed url

--- web/app.js
+++ web/app.js
@@ -2483,6 +2483,7 @@ if (typeof PDFJSDev === "undefined" || PDFJSDev.test("GENERIC")) {
     "https://mozilla.github.io",
     "https://app.solarstream.ch",
     "https://staging.app.solarstream.ch",
+    "https://local.app.solarstream.ch",
   ]);
 
   // eslint-disable-next-line no-var
